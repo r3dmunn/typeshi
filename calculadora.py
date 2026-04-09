87dc7d4f01d2a7fdb372662734ebda283fabc259
Santino Rojas <santinorojasfarias175@gmail.com>
2026-04-08 21:14:23 -0300

    Add files via upload
    empecé con una UI de ctkinter, va queriendo

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,20 @@
 import time as time
+import customtkinter as ctk
 
-print("Bienvenido a una calculadora.\n")
+ventana = ctk.CTk()
+ventana.geometry("400x550")
+
+fuente = ctk.CTkFont(
+    family = "Roboto",
+    size= 16,
+    slant= "italic"
+)
+
+intro = ctk.CTkLabel(ventana,text="redmunn's CALCULAT0R.", font= fuente, fg_color="teal",corner_radius=12)
+intro.pack(padx= 20,pady= 20)
 
 while(True):
-    
+    ventana.mainloop()
     salida = input("Si desea salir, porfavor escriba: SALIR\n")
     salida = salida.upper()
     if (salida != "SALIR"):
@@ -24,7 +35,6 @@ while(True):
 
     
     if (salida == "SALIR"):
-        time.sleep(1)    
         break
         
 
